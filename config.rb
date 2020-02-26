128a42c0e5d2412972d6b694866612f077554f85
phu5554375 <phu5554375@gmail.com>
2020-02-26 11:10:18 +0700

    a

---
+++ config.rb
@@ -0,0 +1,7 @@
+http_path 			= "/"
+css_dir 				= "css"
+sass_dir 				= "scss"
+cache 					= true
+output_style 		= :nested # :expanded :nested :compact :compressed
+line_comments 	= false
+sourcemap 			= true
